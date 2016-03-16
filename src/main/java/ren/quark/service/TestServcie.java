fb90a33917a07e4f0b1e8d66146e91d59ce9d66e
zhangjie <zj7816@163.com>
2016-03-16 12:03:13 +0800

    init project

--- src/main/java/ren/quark/service/TestServcie.java
+++
@@ -1,1 +0,0 @@
-/** *dianping.com Inc *Copyright(c)2004-2016 All Rights Reserved. */package ren.quark.service;/** * @author zhangjie * @version $Id: TestServcie.java, v 0.1 2016-03-16 上午11:07 zhangjie Exp $$ */public class TestServcie {    public String name() {        System.out.println("this is zj7816 speaking");        return "zj7816";    }}
